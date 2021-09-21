70bf3a26f89cb0156c46960a81dde8514525faef
lrod <lrod@lrod-pc>
2021-09-21 16:18:10 -0500

    Added config containers

---
+++ src/com/bacon/gui/BoxContain.java
@@ -0,0 +1,15 @@
+package com.bacon.gui;
+
+import javax.swing.Box;
+
+/**
+ *
+ * @author lrod
+ */
+public class BoxContain extends Box     {
+
+    public BoxContain(int axis) {
+        super(axis);
+    }
+
+}
